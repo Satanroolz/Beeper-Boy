433b601d1208af80fe380a074c1014d9af4eb900
Sean <sean.coffman@icloud.com>
2024-05-26 08:29:29 -0500

    initial commit

---
+++ main.rb
@@ -0,0 +1,20 @@
+require 'discordrb'
+config = File.foreach('config.txt').map { |line| line.split(' ').join(' ') }
+token = config[0].to_s
+bot = Discordrb::Commands::CommandBot.new token: "#{token}", client_id: "#{config[1].to_s}", prefix: "#{config[2].to_s}"
+
+bot.command :ping do |msg|
+    msg.respond "pong."
+end
+
+bot.command(:random, min_args: 0, max_args: 2, description: 'Generates a random number between 0 and 1, 0 and max or min and max.', usage: 'random [min/max] [max]') do |_event, min, max|
+    if max
+      rand(min.to_i..max.to_i)
+    elsif min
+      rand(0..min.to_i)
+    else
+      rand
+    end
+  end
+
+bot.run
