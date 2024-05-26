1b33ce5bb1c128177098e1b95d7ed3a6aba46caf
Sean <sean.coffman@icloud.com>
2024-05-26 08:42:04 -0500

    added response time to ping

--- main.rb
+++ main.rb
@@ -3,9 +3,10 @@ config = File.foreach('config.txt').map { |line| line.split(' ').join(' ') }
 token = config[0].to_s
 bot = Discordrb::Commands::CommandBot.new token: "#{token}", client_id: "#{config[1].to_s}", prefix: "#{config[2].to_s}"
 
-bot.command :ping do |msg|
-    msg.respond "pong."
-end
+bot.message(content: '!ping') do |event|
+    m = event.respond('Pong!')
+    m.edit "Pong! Time taken: #{Time.now - event.timestamp} seconds."
+  end
 
 bot.command(:random, min_args: 0, max_args: 2, description: 'Generates a random number between 0 and 1, 0 and max or min and max.', usage: 'random [min/max] [max]') do |_event, min, max|
     if max
